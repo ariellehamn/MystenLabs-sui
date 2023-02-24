c6995901433bf53564397d0e47004bcf0018ebee
Mark Logan <mark@mystenlabs.com>
2023-02-23 16:13:09 -0800

    Basic mechanics for voting on framework upgrades

--- crates/sui-types/src/messages.rs
+++ crates/sui-types/src/messages.rs
@@ -186,6 +186,11 @@ pub struct ChangeEpoch {
     pub storage_rebate: u64,
     /// Unix timestamp when epoch started
     pub epoch_start_timestamp_ms: u64,
+    /// System packages (specifically framework and move stdlib) that are written before the new
+    /// epoch starts. This tracks framework upgrades on chain. When executing the ChangeEpoch txn,
+    /// the validator must write out the specified objects below.
+    /// If None, no upgrades were performed this epoch.
+    pub system_packages: Option<Vec<ObjectRef>>,
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Serialize, Deserialize)]
@@ -1613,6 +1618,7 @@ impl VerifiedTransaction {
         computation_charge: u64,
         storage_rebate: u64,
         epoch_start_timestamp_ms: u64,
+        system_packages: Option<Vec<ObjectRef>>,
     ) -> Self {
         ChangeEpoch {
             epoch: next_epoch,
@@ -1621,6 +1627,7 @@ impl VerifiedTransaction {
             computation_charge,
             storage_rebate,
             epoch_start_timestamp_ms,
+            system_packages,
         }
         .pipe(SingleTransactionKind::ChangeEpoch)
         .pipe(Self::new_system_transaction)
@@ -2870,8 +2877,13 @@ pub struct AuthorityCapabilities {
     /// (Currently, we just set this to the current time in milliseconds since the epoch, but this
     /// should not be interpreted as a timestamp.)
     pub generation: u64,
+
     /// ProtocolVersions that the authority supports.
     pub supported_protocol_versions: SupportedProtocolVersions,
+
+    /// The ObjectRefs of all versions of system packages that the validator possesses.
+    /// Used to determine whether to do a framework/movestdlib upgrade.
+    pub available_system_packages: Vec<ObjectRef>,
 }
 
 impl Debug for AuthorityCapabilities {
@@ -2891,6 +2903,7 @@ impl AuthorityCapabilities {
     pub fn new(
         authority: AuthorityName,
         supported_protocol_versions: SupportedProtocolVersions,
+        available_system_packages: Vec<ObjectRef>,
     ) -> Self {
         let generation = SystemTime::now()
             .duration_since(UNIX_EPOCH)
@@ -2902,6 +2915,7 @@ impl AuthorityCapabilities {
             authority,
             generation,
             supported_protocol_versions,
+            available_system_packages,
         }
     }
 }
